91eb6482d51f2402f62939d7fb615b57cf473f5d
arnasRad <arnasrad@gmail.com>
2019-11-24 22:33:02 +0200

    minor verification graph and layout changes

--- ForwardChaining/src/com/arnasrad/fbchaining/graph/VerificationGraph.java
+++ ForwardChaining/src/com/arnasrad/fbchaining/graph/VerificationGraph.java
@@ -3,6 +3,8 @@ package com.arnasrad.fbchaining.graph;
 import com.arnasrad.fbchaining.MainController;
 import com.arnasrad.fbchaining.layout.Layout;
 import com.arnasrad.fbchaining.layout.VerificationLayout;
+import com.arnasrad.fbchaining.model.Edge;
+import com.arnasrad.fbchaining.model.Model;
 import com.arnasrad.fbchaining.model.Rule;
 import com.arnasrad.fbchaining.utility.Utils;
 
@@ -45,8 +47,26 @@ public class VerificationGraph extends Graph {
 
     private void addFact(String fact) {
 
-        this.facts.add(fact);
-        layout.relocateLastVertex();
+        try {
+            String newVertexId = getAppendedFactString(fact);
+            this.facts.add(fact);
+
+            // TODO: fix vertex addition
+            Model model = getModel();
+            model.addVertex(newVertexId);
+            endUpdate();
+
+            beginUpdate();
+            model.addEdge(this.facts.get(this.facts.size()-1),
+                    newVertexId, true, Edge.NULL_COST);
+
+            layout.relocateLastVertex();
+        } catch (Exception e) {
+            System.err.println("ERROR: error occurred while adding a new edge. " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            endUpdate();
+        }
     }
 
     public void apply(Rule rule) {
@@ -63,5 +83,11 @@ public class VerificationGraph extends Graph {
     private String getFactString() {
 
         return Utils.getListString(this.facts, "\n");
+    }
+
+    private String getAppendedFactString(String fact) {
+
+        String facts = Utils.getListString(this.facts, "\n");
+        return facts.concat("\n").concat(fact);
     }
 }
